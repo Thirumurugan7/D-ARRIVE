e1dd81c32634bdad4d067525f926b222482f11ee
Thirumurugan <thiru82003@gmail.com>
2022-08-27 13:01:15 +0530

    added maps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Navbar from "../components/Navbar";
 import Map from "../components/Map";
+import LocationSelector from "../components/LocationSelector";
 const style = {
   wrapper: `h-screen w-screen flex flex-col`,
+  main: `h-full w-screen flex-1 z-10`,
+  mapContainer: `flex-1 w-full h-full`,
+  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
 };
 
 export default function Home() {
@@ -10,12 +15,12 @@ export default function Home() {
       {/* navbar */}
       <Navbar />
       <div className={style.main}>
-        <br /> <Map />
+        <Map />
       </div>
       <div className={style.rideRequestContainer}>
         <div className={style.rideRequest}>
-          {/* location selector
-          confirm ride */}
+          <LocationSelector />
+          {/* confirm ride  */}
         </div>
       </div>
     </div>
